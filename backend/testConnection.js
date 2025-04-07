bab4f86fa7240f76e699ad783676b0f6c1bbef51
sriyavemuri <vemurisriya@gmail.com>
2025-04-07 00:23:03 -0400

    Removed testConnection.js

--- backend/testConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://admin001:mongoose$cooks@cluster0.u3c52.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected!'))
-.catch(err => console.log('Connection Error:', err));
