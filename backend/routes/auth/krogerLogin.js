fa66aa09d01fc48f815a4e1b592d2d39a01dcc10
Dhruvita-P <157756435+Dhruvita-P@users.noreply.github.com>
2025-04-07 13:56:47 -0400

    Kroger first push

---
+++ backend/routes/auth/krogerLogin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const { KROGER_CLIENT_ID, KROGER_REDIRECT_URI } = process.env;
+  const scope = 'cart.basic profile.product product.compact';
+
+  const authURL = `https://api.kroger.com/v1/connect/oauth2/authorize?client_id=${KROGER_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(KROGER_REDIRECT_URI)}&scope=${encodeURIComponent(scope)}`;
+
+  res.redirect(authURL);
+});
+
+module.exports = router;
