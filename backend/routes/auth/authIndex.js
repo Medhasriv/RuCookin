8a7cc7c44953218a01b1d10d555a800201ca1efc
Dhruvita-P <157756435+Dhruvita-P@users.noreply.github.com>
2025-05-05 21:50:16 -0400

    Update authIndex.js

--- backend/routes/auth/authIndex.js
+++ backend/routes/auth/authIndex.js
@@ -4,17 +4,13 @@ const router = express.Router();
 // Get all routes from auth
 const signupRoute = require('./signup');
 const loginRoute = require('./login');
-const krogerLoginRoute = require('./krogerLogin');
-const krogerCallbackRoute = require('./krogerCallback');
 const adminCreateAccountRoute = require('./adminCreateAccount');
 const profileRoute = require('./profile');
 
 // Use all the routes
 router.use('/signup', signupRoute);
 router.use('/login', loginRoute);
-router.use('/krogerLogin', krogerLoginRoute);
 router.use('/adminCreateAccount', adminCreateAccountRoute)
-router.use('/krogerCallback', krogerCallbackRoute);
 router.use('/profile', profileRoute);
 
 module.exports = router;
